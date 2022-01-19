88d4a23707c5a8edb9a24d385ed2b28882ae718b
Dario Welcome <welcomedario9@gmail.com>
2022-01-19 15:10:30 -0600

    Actualize mouse-input.c cambio de color de la bola

--- mouse-input.c
+++ mouse-input.c
@@ -21,17 +21,26 @@ int main()
     // Main game loop
     while(!WindowShouldClose())
     {
-	// Update
-	ballPosition = GetMousePosition();
+        // Update
+        ballPosition = GetMousePosition();
+        
+        if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) ballColor = DARKBLUE;
+        else if(IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)) ballColor = LIME;
+        else if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) ballColor = RED;
 
+        // Draw
+        BeginDrawing();
+          
+            ClearBackground(RAYWHITE);
+            DrawCircleV(ballPosition, 50, ballColor);
 
-	// Draw
-	BeginDrawing();
-	    
-	    ClearBackground(RAYWHITE);
-	    DrawCircleV(ballPosition, 50, ballColor);
+            DrawRectangle(5, 5, 230, 50, Fade(SKYBLUE, 0.5f));
+            DrawRectangleLines(5, 5, 230, 50, BLUE);
+            DrawText("INSTRUCCIONES:", 15, 10, 10, BLACK);
+            DrawText("MUEVA LA BOLA CON EL MOUSE", 15, 25, 10, DARKGRAY);
+            DrawText("HAGA CLICK PARA CAMBIAR EL COLOR", 15, 40, 10, DARKGRAY);
 
-	EndDrawing();
+        EndDrawing();
     }
 
 }
